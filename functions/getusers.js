c310e660e93c33f5a6d306be47eed27be2f6dd00
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 09:33:39 -0400

    index.html and main.js

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,8 +1,36 @@
-exports.handler = function(event, context, callback) {
-  const { name } = JSON.parse(event.body);
+const axios = require('axios');
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello ' + name })
-  });
+exports.handler = function(event, context, callback) {
+  const API_URL = 'https://api.github.com/users';
+  const API_CLIENT_ID = '129b591c3d84ede841b5';
+  const API_CLIENT_SECRET = '6a83a5976eb8506b446cd0b3b360d85e76cf5d4a';
+
+  const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
+
+  // Send user response
+  const send = body => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      body: JSON.stringify(body)
+    });
+  };
+
+  // Perform API Call
+  const getUsers = () => {
+    axios
+      .get(URL)
+      .then(res => send(res.data))
+      .catch(err => send(err));
+  };
+
+  // Make sure method is GET
+  if (event.httpMethod == 'GET') {
+    // Run
+    getUsers();
+  }
 };
