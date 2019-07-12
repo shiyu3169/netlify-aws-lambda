262611bdf3c874751ed9dfebc9e480f54aab9e19
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 09:37:57 -0400

    build

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
 
 exports.handler = function(event, context, callback) {
-  const API_URL = 'https://api.github.com/users';
-  const API_CLIENT_ID = '129b591c3d84ede841b5';
-  const API_CLIENT_SECRET = '6a83a5976eb8506b446cd0b3b360d85e76cf5d4a';
+  const { API_CLIENT_ID, API_CLIENT_SECRET, API_URL } = process.env;
 
   const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
 
